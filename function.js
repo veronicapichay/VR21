c9e342d9c600d1c0366f0d6acdadbdef3eee8afc
Veronica Pichay <veronicapichay@gmail.com>
2022-12-02 13:24:26 -0800

    interactive js

--- function.js
+++ function.js
@@ -1,125 +1,75 @@
-// const difference = (num1, num2) => {
-//     return num1 - num2;
-// };
-//  console.log(difference(8,2));
 
-//  function difference(a,b){
-//     return a-b;
+const product = (a,b) => {
+    return a*b;
+  };
+  console.log(product(8,2));
+  
+  
+  const difference = (num1, num2) => {
+      return num1 - num2;
+  };
+   console.log(difference(8,2));
+  
+  const printDay = (num) => {
+  const day = {
+        1: "Sunday",
+        2: "Monday",
+        3: "Tuesday",
+        4: "Wednesday",
+        5: "Thursday",
+        6: "Friday",
+        7: "Saturday",
+      };
     
-//   }
-//   console.log(difference(8,2));
-  
-//   function product(a,b){
-//     return a*b;
-//   }
-  
-//   function printDay(num){
-  
-//     let dates = {
-//       1: "Sunday",
-//       2: "Monday",
-//       3: "Tuesday",
-//       4: "Wednesday",
-//       5: "Thursday",
-//       6: "Friday",
-//       7: "Saturday",
-//     };
-//     return dates[num];
-//   }
-  
-//   function lastElement(arr){
-//     return arr[arr.length-1];
-//   }
-  
-//   function numberCompare(a,b){
-//     if(a === b){
-//       return 'Numbers are equal';
-//     } else if(a > b){
-//       return 'First is greater';
-//     }
-//     return 'Second is greater';
-//   }
-  
-//   function singleLetterCount(str1, letter){
-//     let finalCount = 0;
-//     for(let i=0; i< str1.length; i++){
-//       if(str1[i].toLowerCase() === letter.toLowerCase()){
-//         finalCount++;
-//       }
-//     }
-//     return finalCount;
-//   }
-  
-//   function multipleLetterCount(str){
-//     str = str.toLowerCase();
-//     let finalObj = {};
-//     for(let i =0; i< str.length; i++){
-//       if (finalObj[str[i]] === undefined){
-//         finalObj[str[i]] = 1;
-//       } else {
-//         finalObj[str[i]]++;
-//       }
-//     }
-//     return finalObj;
-//   }
-  
-//   function arrayManipulation(arr, command, position, val){
-//     if(command === 'remove'){
-//       if(position === 'end'){
-//         return arr.pop();
-//       }
-//         return arr.shift();
-//     }
-//     else if(command === 'add'){
-//       if(position === 'end'){
-//         arr.push(val)
-//         return arr;
-//       }
-//       arr.unshift(val)
-//       return arr;
-//     }
-//   }
-  
-//   function isPalindrome(str){
-//     return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
-  
-//     // for(let i =0; i<str.length/2; i++){
-//     //   if(str[i].toLowerCase() !== str[str.length-1-i].toLowerCase()){
-//     //     return false;
-//     //   }
-//     // }
-//     // return true;
-//   }
-  
-//   function RPS(){
-  
-//     function determineComputer(num){
-//       if(num <= .33) return "rock";
-//       else if(num <= .66) return "paper";
-//       return "scissor";
-//     }
-  
-//     let userChoice = prompt("Choose rock / paper or scissor").toLowerCase();
-//     let computerChoice = determineComputer(Math.random());
+    return day[num];  
+  }
+  console.log(printDay(4));
+  console.log(printDay(41));
   
   
-//     let answers = ["rock", "paper", "scissor"];
+  function lastElement(arr){
+      return arr.pop();
   
-//     if(!userChoice || answers.indexOf(userChoice) === -1){
-//       return "Please select a valid option";
-//     }
+    } 
+    console.log(lastElement([1,2,3,9])); // 4
+    console.log(lastElement([])); // undefined
   
-//     if(userChoice === computerChoice) return "Tie!";
   
-//     if(userChoice === "rock" && computerChoice === "paper") {
-//       return "You lose, computer picked " +  computerChoice;
-//     }
-//     if(userChoice === "paper" && computerChoice === "scissor") {
-//       return "You lose, computer picked " +  computerChoice;
-//     }
-//     if(userChoice === "scissor" && computerChoice === "rock") {
-//       return "You lose, computer picked " +  computerChoice;
-//     }
+  function numberCompare(a, b) {
+    
+      if (a === b) {
+          return "Numbers are equal";
+      } else if (a > b) {
+          return "First is greater"
+      } else {
+          return "Second is greater"
+      }
+  }
   
-//     return "You win! Computer picked " +  computerChoice;
-//   }
+  console.log(numberCompare(1, 1)); // "Numbers are equal"
+  console.log(numberCompare(2, 1)); // "First is greater"
+  console.log(numberCompare(1, 2)); // "Second is greater"
+  
+  var name = prompt("What's up shorty?");
+  
+  // const singleLetter = (word, singleLetter) => {
+  //   word = word.toUpperCase();
+  //   letter = letter.toUpperCase();
+  
+  //   if(!word.match(new))
+  
+    
+  // }
+  
+  
+  
+    function isPalindrome(str){
+      return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
+    
+      for(let i =0; i<str.length/2; i++){
+        if(str[i].toLowerCase() !== str[str.length-1-i].toLowerCase()){
+          return false;
+        }
+      }
+      return true;
+    }
