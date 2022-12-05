b99f49920bbb5212f78023029f38bc6f9983a5a1
Veronica Pichay <veronicapichay@gmail.com>
2022-12-05 10:10:13 -0800

    morning review

---
+++ timer.js
@@ -0,0 +1,30 @@
+let timerId;
+let counter = 0;
+
+function incrementTime () {
+    counter++;
+    document.querySelector(".count").innerText = counter;
+}
+
+
+function start() {
+    if (!timerId) {
+        timerId = setInterval(incrementTime, 1000);
+    }
+}
+            
+
+function stop() {
+    clearInterval(timerId);
+    timerId = null;
+    document.querySelector(".count").innerText = "*";
+}
+
+function reset() {
+    
+    clearInterval(timerId);
+    counter = 0;
+    counter++;
+}
+      
+    
