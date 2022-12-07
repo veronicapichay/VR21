e08ec6887ce1f0221d362d7a123c00e80cb68547
Veronica Pichay <veronicapichay@gmail.com>
2022-12-07 15:24:37 -0800

    memory game exercise

--- app.js
+++ app.js
@@ -1,50 +1,42 @@
+//prevents null pointer reference errors, waits for contents to be loaded before game starts
 document.addEventListener("DOMContentLoaded", function() {
-  const cards = document.querySelectorAll(".game-card");
+  const cards = document.querySelectorAll(".game-card"); //returns an array inherently dynamic
   let numCards = cards.length;
-  let card1 = null;
+  let card1 = null; //bucket to assign what user picked on 
   let card2 = null;
   let cardsFlipped = 0;
-  let currentScore = 0;
-  let lowScore = localStorage.getItem("low-score");
-  let start = document.getElementById("start");
 
-  if (lowScore) {
-    document.getElementById("best-score").innerText = lowScore;
-  }
-
-  for (let card of cards) {
+ //each card invokes the fx 
+  for (let card of cards) { 
     card.addEventListener("click", handleCardClick);
   }
 
   let startBtn = document.getElementById("start-button");
   startBtn.addEventListener("click", startGame);
 
+  //veryfying state of card if not flipped
   function handleCardClick(e) {
-    if (!e.target.classList.contains("front")) return;
-
+    if (!e.target.classList.contains("front")) return; //if statement is met it stops right here 
     let currentCard = e.target.parentElement;
 
-    if (!card1 || !card2) {
-      if (!currentCard.classList.contains("flipped")) {
-        setScore(currentScore + 1);
-      }
+    if (!card1 || !card2) { //if either is false 
       currentCard.classList.add("flipped");
-      card1 = card1 || currentCard;
-      card2 = currentCard === card1 ? null : currentCard;
-    }
+      card1 = card1 || currentCard; 
+      card2 = currentCard === card1 ? null : currentCard; //if c1 equals to current card assign c2 to null else assign c2 to be current card 
 
-    if (card1 && card2) {
-      let gif1 = card1.children[1].children[0].src;
+    if (card1 && card2) { //if both true assign value from gif source 
+      let gif1 = card1.children[1].children[0].src; 
       let gif2 = card2.children[1].children[0].src;
 
-      if (gif1 === gif2) {
+      //matching logic
+      if (gif1 === gif2) { //increment card flipped by 2 and remove listener
         cardsFlipped += 2;
         card1.removeEventListener("click", handleCardClick);
         card2.removeEventListener("click", handleCardClick);
         card1 = null;
         card2 = null;
       } else {
-        setTimeout(function() {
+        setTimeout(function() { //resets the board
           card1.classList.remove("flipped");
           card2.classList.remove("flipped");
           card1 = null;
@@ -52,53 +44,39 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 1000);
       }
     }
-
-    if (cardsFlipped === numCards) endGame();
+  }
+    if (cardsFlipped === numCards) endGame(); //value of length of array numCards
   }
 
   function startGame() {
-    setScore(0);
     start.classList.add("playing");
-    let indices = [];
-    for (let i = 1; i <= numCards / 2; i++) {
-      indices.push(i.toString());
+    let indices = []; //creates an empty arr
+    for (let i = 1; i <= numCards / 2; i++) { 
+      indices.push(i.toString()); //values of i in empt arr, strings 
     }
-    let pairs = shuffle(indices.concat(indices));
-
+    let pairs = shuffle(indices.concat(indices)); //creating indices of arr
     for (let i = 0; i < cards.length; i++) {
-      let path = "gifs/" + pairs[i] + ".gif";
+      let path = pairs[i] + ".gif";
       cards[i].children[1].children[0].src = path;
     }
   }
 
   function shuffle(array) {
-    let arrayCopy = array.slice();
-    for (let idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
+    let arrayCopy = array.slice(); //copy of the input array
+    //end of the array - decrementing the array backwards
+    for (let idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) { 
       // generate a random index between 0 and idx1 (inclusive)
-      let idx2 = Math.floor(Math.random() * (idx1 + 1));
+      let idx2 = Math.floor(Math.random() * (idx1 + 1)); //random numbers between range of the array, assigning where it should go
 
       // swap elements at idx1 and idx2
-      let temp = arrayCopy[idx1];
+      let temp = arrayCopy[idx1]; 
       arrayCopy[idx1] = arrayCopy[idx2];
       arrayCopy[idx2] = temp;
     }
     return arrayCopy;
   }
 
-  function setScore(newScore) {
-    currentScore = newScore;
-    document.getElementById("current-score").innerText = currentScore;
-  }
-
   function endGame() {
-    let end = document.getElementById("end");
-    let scoreHeader = end.children[1];
-    scoreHeader.innerText = "Your score: " + currentScore;
-    let lowScore = +localStorage.getItem("low-score") || Infinity;
-    if (currentScore < lowScore) {
-      scoreHeader.innerText += " - NEW BEST SCORE!!";
-      localStorage.setItem("low-score", currentScore);
-    }
     document.getElementById("end").classList.add("game-over");
   }
 });
