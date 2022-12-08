798c734e9b51dbaec1f941e3c7f9c90e6c44e14c
Veronica Pichay <veronicapichay@gmail.com>
2022-12-08 13:50:38 -0800

    sample jasmine tests

--- bootstrap.js
+++ bootstrap.js
@@ -1,43 +1,120 @@
-document
-    .querySelector("#save").addEventListener("click", (e) =>  {
-      const contentCard = `<div class="col-lg-3 col-md-4 col-sm-6 d-flex">
-                <div class="card">
-                  <img src = "https://www.yankodesign.com/images/design_news/2022/12/these_headphones_lets_you_feel_the_music_layout.jpg"
-                    class="card-img-top"
-                    alt="..."
-                  />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and make up
-                      the bulk of the card's content 
-                    </p>
-                    <div>
-                      <button
-                        type="button"
-                        class="btn btn-primary"
-                        data-bs-toggle="modal"
-                        data-bs-target="#updateModal"
-                      >
-                        Update
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-primary"
-                        data-bs-toggle="modal"
-                        data-bs-target="#deleteModal"
-                      >
-                        Delete
-                      </button>
+const addCard = document.querySelector("#createModal form");
+console.log(addCard); //debugging purposes
+console.log("add form added");
+addCard.addEventListener("submit", handleAddSubmit);
+
+//submit fx
+function handleAddSubmit(event) {
+  event.preventDefault(); //refresh is a default behavior
+  console.log(addCard.elements);
+  console.log("Success! Form submitted."); //debugging purposes
+
+  //accessing values in ea elements using name properties
+  const memeUrl = addForm.elements.imageUrl.value;
+  const top = addForm.elements.title.value;
+  const bottom = addForm.elements.bottom.value;
+  //console.log(memeUrl, top, bottom);
+
+  const cardInfo = {imageUrl, title, bottom};
+  //call fx passing an obj cardInfo to avoid returning wrong data 
+  createDesignCard(cardInfo);
+
+  //clears the form for new entry
+  addCard.reset();
+
+  const closeBtn = document.querySelector('[data-bs-dismiss="modal"]') //selects Close btn after form submit
+  closeBtn.click();
+}
+
+//meme card html template
+function createDesignCard (cardInfo) {
+  const colCard = document.createElement("div");
+    colCard.classList.add("col");
+    colCard.innerHTML =
+  const colCard = 
+   document
+      .querySelector("#save").addEventListener("click", (e) =>  {
+        const contentCard = `<div class="col-lg-3 col-md-4 col-sm-6 d-flex">
+                  <div class="card">
+                    <img src = "https://www.yankodesign.com/images/design_news/2022/12/these_headphones_lets_you_feel_the_music_layout.jpg"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">
+                        Some quick example text to build on the card title and make up
+                        the bulk of the card's content 
+                      </p>
+                      <div>
+                        <button
+                          type="button"
+                          class="btn btn-primary"
+                          data-bs-toggle="modal"
+                          data-bs-target="#updateModal"
+                        >
+                          Update
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-primary"
+                          data-bs-toggle="modal"
+                          data-bs-target="#deleteModal"
+                        >
+                          Delete
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>`;
+                </div>`;
+  
+                document
+                  .querySelector(".row")
+                  .insertAdjacentHTML("beforeend", contentCard);
+    });
 
-              document
-                .querySelector(".row")
-                .insertAdjacentHTML("beforeend", contentCard);
-  });
+
+
+// document
+//     .querySelector("#save").addEventListener("click", (e) =>  {
+//       const contentCard = `<div class="col-lg-3 col-md-4 col-sm-6 d-flex">
+//                 <div class="card">
+//                   <img src = "https://www.yankodesign.com/images/design_news/2022/12/these_headphones_lets_you_feel_the_music_layout.jpg"
+//                     class="card-img-top"
+//                     alt="..."
+//                   />
+//                   <div class="card-body">
+//                     <h5 class="card-title">Card title</h5>
+//                     <p class="card-text">
+//                       Some quick example text to build on the card title and make up
+//                       the bulk of the card's content 
+//                     </p>
+//                     <div>
+//                       <button
+//                         type="button"
+//                         class="btn btn-primary"
+//                         data-bs-toggle="modal"
+//                         data-bs-target="#updateModal"
+//                       >
+//                         Update
+//                       </button>
+//                       <button
+//                         type="button"
+//                         class="btn btn-primary"
+//                         data-bs-toggle="modal"
+//                         data-bs-target="#deleteModal"
+//                       >
+//                         Delete
+//                       </button>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>`;
+
+//               document
+//                 .querySelector(".row")
+//                 .insertAdjacentHTML("beforeend", contentCard);
+//   });
 
 
 
