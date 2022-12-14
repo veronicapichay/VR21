36fc7c7e6385d902d6a0c25f7aebf7e486f6257e
Veronica Pichay <veronicapichay@gmail.com>
2022-12-13 21:45:09 -0800

    updates from mac

--- apiunsplash.js
+++ apiunsplash.js
@@ -1,29 +1,19 @@
-// const client_id = "qTvoYhj5lwBIculuHLXUAFLgm2X6DaZfPJB6Yxxkn0A"; // Create an account unsplash tp hey client ID;
-// const searchBtn = document.getElementById("input").valuel
-// let searchVal;
+const client_id = "qTvoYhj5lwBIculuHLXUAFLgm2X6DaZfPJB6Yxxkn0A"; // Create an account unsplash tp hey client ID;
+const searchBtn = "electric fan";
+const resource = `https://api.unsplash.com/search/photos?query=${searchBtn}&per_page=22&client_id=${client_id}`
 
-// fetch(
-//   `https://api.unsplash.com/search/photos?query=${searchStr}&per_page=20&client_id=${client_id}`
-// )
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const container = document.querySelector(".container");
+fetch(resource)
+  .then((response) => response.json())
+  .then((data) => {
+    const container = document.querySelector("#container");
 
-//     for (let i = 0; i < data.results.length - 1; i++) {
-//       const img = document.createElement("img");
+    for (let i = 0; i < data.results.length - 1; i++) {
+      const url = data.results[i].urls.thumb;
+      const img = document.createElement("img");
 
-//       img.setAttribute("src", data.results[i].urls.thumb);
+      img.setAttribute("src", url);
+      container.append(img);
+    }
+  });
 
-//       container.append(img);
-//     }
-//   });
-
-  const clearInput = () => {
-    const input = document.getElementsByTagName("input")[0];
-    input.value = "";
-  }
-  
-  const clearBtn = document.getElementById("clear-btn");
-  clearBtn.addEventListener("click", clearInput);
-  
   
